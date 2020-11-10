576fd153eef6b17851d2fec554491abf409bd5cc
Vishal9860 <vishaljare11@gmail.com>
2020-11-09 21:21:08 -0800

    first commit by vishal

---
+++ app/src/main/java/com/vishal/myapplication/SplashScreen.java
@@ -0,0 +1,23 @@
+package com.vishal.myapplication;
+
+import android.content.Intent;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.vishal.myapplication.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+            }
+        }, 1500);
+    }
+}
